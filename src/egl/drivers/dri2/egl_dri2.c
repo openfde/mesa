43520e58baa3beecc37cb8247c2b679925264683
Haixia Shi <hshi@chromium.org>
2014-12-05 11:00:59 -0800

    egl/dri2: implement platform_null.
    BUG=chromium:432325
    TEST=force mesa to use platform_null and verify with test app.
    
    Change-Id: If72f8a49dd9b7370c90619bf52ca8c773d6ce377

--- src/egl/drivers/dri2/egl_dri2.c
+++ src/egl/drivers/dri2/egl_dri2.c
@@ -639,6 +639,13 @@ dri2_initialize(_EGLDriver *drv, _EGLDisplay *disp)
       return EGL_FALSE;
 
    switch (disp->Platform) {
+#ifdef HAVE_NULL_PLATFORM
+   case _EGL_PLATFORM_NULL:
+      if (disp->Options.TestOnly)
+         return EGL_TRUE;
+      return dri2_initialize_null(drv, disp);
+#endif
+
 #ifdef HAVE_X11_PLATFORM
    case _EGL_PLATFORM_X11:
       if (disp->Options.TestOnly)
@@ -1578,7 +1585,7 @@ dri2_create_wayland_buffer_from_image(_EGLDriver *drv, _EGLDisplay *dpy,
    return dri2_dpy->vtbl->create_wayland_buffer_from_image(drv, dpy, img);
 }
 
-#ifdef HAVE_LIBDRM
+#if defined(HAVE_LIBDRM) || defined(HAVE_NULL_PLATFORM)
 static EGLBoolean
 dri2_check_dma_buf_attribs(const _EGLImageAttribs *attrs)
 {
@@ -1836,7 +1843,7 @@ dri2_create_image_khr(_EGLDriver *drv, _EGLDisplay *disp,
    case EGL_WAYLAND_BUFFER_WL:
       return dri2_create_image_wayland_wl_buffer(disp, ctx, buffer, attr_list);
 #endif
-#ifdef HAVE_LIBDRM
+#if defined(HAVE_LIBDRM) || defined(HAVE_NULL_PLATFORM)
    case EGL_LINUX_DMA_BUF_EXT:
       return dri2_create_image_dma_buf(disp, ctx, buffer, attr_list);
 #endif
